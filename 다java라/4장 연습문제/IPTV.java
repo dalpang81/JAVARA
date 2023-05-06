e813ae5c4ed09081f3f7fda4e5dcd89fc32548c3
kidreamx <kidreamx@gmail.com>
2023-05-07 00:46:20 +0900

    5장 2번문제까지 클리어

---
+++ 다java라/4장 연습문제/IPTV.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+class TV{
+	private int size;
+	public TV(int size) {this.size = size;}
+	protected int getSize() { return size;}
+}
+class ColorTV extends TV{
+	private int  color;
+	public ColorTV(int inch, int color) {
+		super(inch);
+		this.color = color;
+	}
+	protected int getColor() {return color;}
+	public void printProperty() {
+		System.out.print(getSize()+"인치 "+color+"컬러");
+	}
+}
+
+public class IPTV extends ColorTV {
+	private String ip;
+	public IPTV(String ip, int inch, int color) {
+		super(inch,color);
+		this.ip = ip;
+	}
+	public void printProperty() {
+		System.out.print("나의 IPTV는 "+ ip+ " 주소의 ");
+		super.printProperty();
+	}
+	public static void main(String[] args) {
+		IPTV iptv = new IPTV("192.1.1.2",32,2048);
+		iptv.printProperty();
+
+	}
+
+}
